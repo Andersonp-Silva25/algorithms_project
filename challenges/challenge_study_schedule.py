1582470968de7a19813ba66200fbeb9680417f0c
Anderson Pereira da Silva <andersonp.silva25@gmail.com>
2023-04-25 17:04:09 -0300

    :bug: Fix: Fixing linter bugs

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,10 +1,10 @@
 def study_schedule(permanence_period, target_time):
     if not target_time:
         return None
-    
+
     counter = 0
 
-    for entry_period, leave_period  in permanence_period:
+    for entry_period, leave_period in permanence_period:
         if isinstance(entry_period, int) and isinstance(leave_period, int):
             if entry_period <= target_time <= leave_period:
                 counter += 1
