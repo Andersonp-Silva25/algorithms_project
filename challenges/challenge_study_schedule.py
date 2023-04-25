cd9b0ea186b6f408a5aa7fdd5dcf82b6db7bd7e7
Anderson Pereira da Silva <andersonp.silva25@gmail.com>
2023-04-25 16:57:51 -0300

    :sparkles: Feat: Algorithm that searches for number of students studying at the same time

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not target_time:
+        return None
+    
+    counter = 0
+
+    for entry_period, leave_period  in permanence_period:
+        if isinstance(entry_period, int) and isinstance(leave_period, int):
+            if entry_period <= target_time <= leave_period:
+                counter += 1
+        else:
+            return None
+    return counter
